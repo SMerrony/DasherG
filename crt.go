fd0b12dc96ae91fc41d84d801d101d45e138a22b
SMerrony <merrony@gmail.com>
2021-10-03 08:12:12 +0200

    Backport supoorting changes for Issue #12

--- crt.go
+++ crt.go
@@ -30,9 +30,9 @@ import (
 )
 
 func buildCrt2() (crtImage *canvas.Raster, backingImage *image.NRGBA) {
-	backingImage = image.NewNRGBA(image.Rect(0, 0, terminal.visibleCols*charWidth, terminal.visibleLines*charHeight))
+	backingImage = image.NewNRGBA(image.Rect(0, 0, terminal.display.visibleCols*charWidth, terminal.display.visibleLines*charHeight))
 	crtImage = canvas.NewRasterFromImage(backingImage) // canvas.NewImageFromImage(backingImage)
-	crtImage.SetMinSize(fyne.Size{float32(terminal.visibleCols * charWidth), float32(terminal.visibleLines * charHeight)})
+	crtImage.SetMinSize(fyne.Size{float32(terminal.display.visibleCols * charWidth), float32(terminal.display.visibleLines * charHeight)})
 	//crtImage.FillMode = canvas.ImageFillOriginal
 	return crtImage, backingImage
 }
@@ -43,26 +43,26 @@ func drawCrt() {
 		// fmt.Println("DEBUG: drawCrt2 running update...")
 		var cIx int
 
-		for line := 0; line < terminal.visibleLines; line++ {
-			for col := 0; col < terminal.visibleCols; col++ {
-				if terminal.displayDirty[line][col] || (terminal.blinkEnabled && terminal.display[line][col].blink) {
-					cIx = int(terminal.display[line][col].charValue)
+		for line := 0; line < terminal.display.visibleLines; line++ {
+			for col := 0; col < terminal.display.visibleCols; col++ {
+				if terminal.displayDirty[line][col] || (terminal.blinkEnabled && terminal.display.cells[line][col].blink) {
+					cIx = int(terminal.display.cells[line][col].charValue)
 					if cIx > 31 && cIx < 128 {
-						// fmt.Printf("DEBUG: drawCrt found updatable char <%c>\n", terminal.display[line][col].charValue)
+						// fmt.Printf("DEBUG: drawCrt found updatable char <%c>\n", terminal.display.cells[line][col].charValue)
 						cellRect := image.Rect(col*charWidth, line*charHeight, col*charWidth+charWidth, line*charHeight+charHeight)
 						switch {
-						case terminal.blinkEnabled && terminal.blinkState && terminal.display[line][col].blink:
+						case terminal.blinkEnabled && terminal.blinkState && terminal.display.cells[line][col].blink:
 							draw.Draw(backingImg, cellRect, bdfFont[32].plainImg, image.ZP, draw.Src)
-						case terminal.display[line][col].reverse:
+						case terminal.display.cells[line][col].reverse:
 							draw.Draw(backingImg, cellRect, bdfFont[cIx].revImg, image.ZP, draw.Src)
-						case terminal.display[line][col].dim:
+						case terminal.display.cells[line][col].dim:
 							draw.Draw(backingImg, cellRect, bdfFont[cIx].dimImg, image.ZP, draw.Src)
 						default:
 							draw.Draw(backingImg, cellRect, bdfFont[cIx].plainImg, image.ZP, draw.Src)
 						}
 					}
 					// underscore?
-					if terminal.display[line][col].underscore {
+					if terminal.display.cells[line][col].underscore {
 						for x := 0; x < charWidth; x++ {
 							backingImg.Set(col*charWidth+x, ((line+1)*charHeight)-1, green)
 						}
@@ -72,13 +72,13 @@ func drawCrt() {
 			} // end for col
 		} // end for line
 		// draw the cursor - if on-screen
-		if terminal.cursorX < terminal.visibleCols && terminal.cursorY < terminal.visibleLines {
+		if terminal.cursorX < terminal.display.visibleCols && terminal.cursorY < terminal.display.visibleLines {
 			cellRect := image.Rect(terminal.cursorX*charWidth, terminal.cursorY*charHeight, terminal.cursorX*charWidth+charWidth, terminal.cursorY*charHeight+charHeight)
-			cIx := int(terminal.display[terminal.cursorY][terminal.cursorX].charValue)
+			cIx := int(terminal.display.cells[terminal.cursorY][terminal.cursorX].charValue)
 			if cIx == 0 {
 				cIx = 32
 			}
-			if terminal.display[terminal.cursorY][terminal.cursorX].reverse {
+			if terminal.display.cells[terminal.cursorY][terminal.cursorX].reverse {
 				draw.Draw(backingImg, cellRect, bdfFont[cIx].plainImg, image.ZP, draw.Src)
 			} else {
 				// fmt.Printf("Drawing cursor at %d,%d\n", terminal.cursorX*charWidth, terminal.cursorY*charHeight)
@@ -88,13 +88,13 @@ func drawCrt() {
 		}
 		// shade any selected area
 		if selectionRegion.isActive {
-			startCharPosn := selectionRegion.startCol + selectionRegion.startRow*terminal.visibleCols
-			endCharPosn := selectionRegion.endCol + selectionRegion.endRow*terminal.visibleCols
+			startCharPosn := selectionRegion.startCol + selectionRegion.startRow*terminal.display.visibleCols
+			endCharPosn := selectionRegion.endCol + selectionRegion.endRow*terminal.display.visibleCols
 			if startCharPosn <= endCharPosn {
 				// normal (forward) selection
 				col := selectionRegion.startCol
 				for row := selectionRegion.startRow; row <= selectionRegion.endRow; row++ {
-					for col < terminal.visibleCols {
+					for col < terminal.display.visibleCols {
 						// drawable.DrawLine(gc, col*charWidth, ((row+1)*charHeight)-1, (col+1)*charWidth-1, ((row+1)*charHeight)-1) // FIXME
 						if row == selectionRegion.endRow && col == selectionRegion.endCol {
 							goto shadingDone
